1c6cf76129e132ad49e24fc61204f5945b007f81
TOMIN PAUL <tominpaul123@gmail.com>
2019-07-14 04:48:33 -0400

    Assignment 3 Compiled Successfully.

--- Assignment_1/Assignment_1/ListOfUsersActivity.cs
+++ Assignment_1/Assignment_1/ListOfUsersActivity.cs
@@ -15,11 +15,60 @@ namespace Assignment_1
     [Activity(Label = "ListOfUsersActivity")]
     public class ListOfUsersActivity : Activity
     {
+        DBHelperClass myDB;
+
+        ListView myList;
+        string[] users;
+        ArrayAdapter<string> Myadapter;
+        SearchView mySearch;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            SetContentView(Resource.Layout.activity_listOfUsers);
+            myDB = new DBHelperClass(this);
+
             // Create your application here
+            mySearch = FindViewById<SearchView>(Resource.Id.searchBtn);
+            myList = FindViewById<ListView>(Resource.Id.listUsers);
+
+            myDB = new DBHelperClass(this);
+            users = myDB.getList();
+
+            // Defined Array values to show in ListView
+            Myadapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, users);
+
+            // Assign adapter to ListView
+            myList.Adapter = Myadapter;
+            myList.ItemClick += List1_ItemClick;
+            mySearch.QueryTextChange += userSearch;
+        }
+
+        private void List1_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Intent userScreen = new Intent(this, typeof(ProfileActivity)); // on success loading signup page   
+            userScreen.PutExtra("userName", users[e.Position]);
+            StartActivity(userScreen);
+        }
+        public void userSearch(object sender, SearchView.QueryTextChangeEventArgs e)
+        {
+            var mySearchValue = e.NewText;
+            string temp;
+            List<string> arrayTemp = new List<string>();
+            for (int i = 0; i < users.Length; ++i)
+            {
+                temp = users[i].ToLower();
+                if (temp.Contains(mySearchValue.ToLower()))
+                {
+                    arrayTemp.Add(users[i]);
+                }
+            }
+            if (arrayTemp.Count > 0)
+            {
+                Myadapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, arrayTemp);
+                myList.Adapter = Myadapter;
+            }
         }
     }
 }
